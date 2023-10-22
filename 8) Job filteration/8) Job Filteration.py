7defe1b7448f9f3bb8756183e08cdcad038a9044
dollarboysushil <thesushilpoudel@gmail.com>
2023-10-23 00:10:55 +0545

    job filteration bot v2 (with text output function)

--- 8) Job filteration/8) Job Filteration.py
+++ 8) Job filteration/8) Job Filteration.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 print("""
       _       _       _____ _ _ _                 _   _               ____        _   
@@ -15,11 +16,13 @@ site used to filter job = https://www.timesjobs.com/
 """)
 skill = input("Enter Skill you have. For multiple skills use comma :")
 skill = skill.replace(",", "+")
-location = input("Enter Location of Job:")
-skill = location.replace("", "+")
+
+location_for_job = input("Enter Location of Job:")
+location_for_job = location_for_job.replace(" ", "+")
+
 
 response = requests.get(
-    f"https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={skill}&txtLocation={location}")
+    f"https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={skill}&txtLocation={location_for_job}")
 
 response = response.text
 
@@ -30,6 +33,8 @@ jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 
 for job in jobs:
 
+    current_time = str(datetime.datetime.today().time()).replace(":", ",")
+    current_time = current_time = current_time.split('.')[0]
     job_title = job.find("a").text
     experience = job.find("li").text.replace("card_travel", '')
     location = job.find("ul", class_="top-jd-dtl clearfix")
@@ -40,7 +45,9 @@ for job in jobs:
     job_link = job.find("header", class_="clearfix").find("a")
     job_link = job_link.get("href")
 
-    with open("txt.txt", "a") as file:
+    file_name = f"{current_time}{skill}{location_for_job}.txt"
+
+    with open(file_name, "a") as file:
         file.write(f"JOB: {job_title.strip()}\n")
         file.write(f"Experience: {experience}\n")
         file.write(f"Location: {location}\n")
@@ -48,12 +55,12 @@ for job in jobs:
         file.write(f"Job Link: {job_link} \n")
         file.write("\n\n")
 
-    print(f"JOB: {job_title.strip()}")
-    print(f"Experience: {experience}")
-    print(f"Location: {location}")
-    print(f"Skills: {key_skills.strip()}")
-    print(f"Job Link: {job_link} ")
-    print("")
-    print("")
+        print(f"JOB: {job_title.strip()}")
+        print(f"Experience: {experience}")
+        print(f"Location: {location}")
+        print(f"Skills: {key_skills.strip()}")
+        print(f"Job Link: {job_link} ")
+        print("")
+        print("")
 
-print("All the results are written in the txt file.")
+print("All the results are written in the txt file with name current hrs/min/sec+skill+location_of_job.txt")
---
+++ 8) Job filteration/temp.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+current_time = "00:02:52.028800"
+print(current_time)
+current_time = current_time.replace(":", ",")
+print(current_time)
+
+
+print(datetime.datetime.today().time())
+current_time = "00:02:52.028800"
+
+# Remove digits after the decimal point
+
+
+print(current_time)
